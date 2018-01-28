8b6b256ef1f140e886fb561499f488f7947d7499
carlosruiz <losman3331984@gmail.com>
2018-01-28 08:55:45 -0800

    asdf

--- project1/settings.py
+++ project1/settings.py
@@ -72,7 +72,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = "https://%s/%s/" % (AWS_STORAGE_BUCKET_NAME, STATICFILES_LOCATION)
+STATIC_URL = "https://%s/%s/" % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
