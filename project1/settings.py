be1123656750e6ffc9e590049e5a6fb8087678cf
carlosruiz <losman3331984@gmail.com>
2018-02-26 07:57:25 -0800

    new

--- project1/settings.py
+++ project1/settings.py
@@ -224,6 +224,8 @@ CMS_PERMISSION = True
 
 CMS_PLACEHOLDER_CONF = {}
 
+DJANGOCMS_STYLE_CHOICES = ['container', 'content', 'teaser', 'cell',]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
